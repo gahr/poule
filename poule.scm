b9b9903d9a2c2fe05c4f262226c67df133c49a08
gahr <gahr>
2023-12-04 09:50:16 +0000

    Improvements

--- poule.scm
+++ poule.scm
@@ -33,8 +33,8 @@
   (define-record job
                  num      ; job number, incrementally allocated
                  status   ; 'pending or 'available
-                 arg      ; argument for the worker function
-                 result   ; result of the worker function
+                 arg      ; argument of the worker function
+                 result   ; result from the worker function
                  )
 
 
@@ -42,19 +42,28 @@
                  pid      ; pid of the child process
                  out      ; parent -> child output port
                  in       ; child -> parent input port
-                 job-num  ; currently assigned job number, or #f if worker is free
+                 job      ; currently assigned job number, or #f if worker is free
                  )
 
   (define-record poule
-                 active?
-                 fn
-                 workers
+                 active?  ; is this poule usable?
+                 fn       ; worker function
+                 workers  ; list of workers
                  submission-thread
-                 queue
-                 jobs
-                 last-job-num
+                 queue    ; incoming jobs, for the submission thread to pick up
+                 jobs     ; pending and available jobs
+                 job-count; 
                  mutex
                  )
+
+  (define worker-free?
+    (complement worker-job))
+
+  (define (worker-assign! w n)
+    (worker-job-set! w n))
+
+  (define (worker-unassign! w)
+    (worker-job-set! w #f))
 
   (define poule-trace (make-parameter #f))
 
@@ -100,22 +109,35 @@
 
   (define (scan-workers p)
     (dp "scan-workers")
-    (filter-map
-      (lambda (w)
-        (and-let* ((n (worker-job-num w))
-                   ((char-ready? (worker-in w)))
-                   (j (find (lambda (j) (eq? (job-num j) n)) (poule-jobs p)))
-                   (r (handle-exceptions exn
-                        (cons #f exn)
-                        (read (worker-in w)))))
-          ; TODO - convert to condition here, so we don't have
-          ; to handle a cons with #t or #f later on?
-          (worker-job-num-set! w #f)
-          (job-status-set! j 'available)
-          (job-arg-set! j #f) ; let it be GC'd
-          (job-result-set! j r)
-          w))
-      (poule-workers p)))
+
+    (define (reap-ready)
+      (filter-map
+        (lambda (w)
+          (and-let* ((n (worker-job w))
+                     ((char-ready? (worker-in w)))
+                     (j (find (lambda (j) (eq? (job-num j) n)) (poule-jobs p)))
+                     (r (handle-exceptions exn
+                          (cons #f exn)
+                          (read (worker-in w)))))
+            ; TODO - convert to condition here, so we don't have
+            ; to handle a cons with #t or #f later on?
+            (worker-unassign! w)
+            (job-status-set! j 'available)
+            (job-arg-set! j #f) ; let it be GC'd
+            (job-result-set! j r)))
+        (poule-workers p)))
+
+    (define (gc-dead)
+      (define (alive? w)
+        (let-values (((pid ok st) (process-wait (worker-pid w) #t)))
+          (zero? pid)))
+      (let-values (((alive dead) (partition alive? (poule-workers p))))
+        (unless (null? dead)
+          (print (length dead) " processes are dead..."))))
+
+    (let ((reaped (reap-ready)))
+      (gc-dead)
+      (not (null? reaped))))
 
   (define (exn->string e)
     (->string (if (condition? e)
@@ -123,6 +145,8 @@
                 e)))
 
   (define (spawn-worker fn)
+    (dp "spawn-worker")
+
     (let-values (((p c) (create-pipe)))
       (define (child)
         (let ((out (open-output-file* c))
@@ -139,23 +163,13 @@
                (write/flush (cons #t #t) out))))))
 
       (match (process-fork child)
-        (0
-         (dp "fork returned 0")
-         (exit))
+        (0 (exit))
         (n
-          (dp "fork returned " n)
+          (dp "spawned workwr pid " n)
           (make-worker n (open-output-file* p) (open-input-file* p) #f)))))
 
   (define (submission p)
-    (define (find-free-worker)
-      (or
-        (and-let* ((w (find (complement worker-job-num) (poule-workers p))))
-          (dp "found free worker " (worker-pid w))
-          w)
-        (and-let* ((w (scan-workers p))
-                   ((not (null? w))))
-          (dp "found worker done " (worker-pid (car w)))
-          (car w))))
+    (dp "submission")
 
     (define (continue?)
       (thread-specific (current-thread)))
@@ -169,11 +183,11 @@
           (j
             (let worker-loop ()
               (cond
-                ((with-synchronized
-                   p
-                   (and-let* ((w (find-free-worker)))
+                ((with-synchronized p
+                   (scan-workers p)
+                   (and-let* ((w (find worker-free? (poule-workers p))))
                      (dp "submission: job " (job-num j) " to worker " (worker-pid w))
-                     (worker-job-num-set! w (job-num j))
+                     (worker-assign! w (job-num j))
                      (write/flush `(work ,(job-arg j)) (worker-out w))
                      #t))
                  (mbox-loop))
@@ -201,24 +215,23 @@
       (poule-submission-thread-set! p t)
       (set-finalizer! p poule-destroy)
       (thread-start! t)
-      p) 
-    )
+      p))
 
   (define (poule-submit p arg)
     (check-poule p 'poule-submit)
 
     (dp "poule-submit " arg)
 
-    (with-synchronized
-      p
-      (let ((j (make-job (add1 (poule-last-job-num p))
+    (with-synchronized p
+      (let* ((n (add1 (poule-job-count p)))
+             (j (make-job n
                          'pending
                          arg
                          #f)))
-        (poule-last-job-num-set! p (job-num j))
+        (poule-job-count-set! p n)
         (poule-jobs-set! p (cons j (poule-jobs p)))
         (mailbox-send! (poule-queue p) j)
-        (job-num j))))
+        n)))
 
   (define (poule-result p num #!optional (wait? #t))
     (check-poule p 'poule-result)
@@ -232,20 +245,19 @@
     ; - (#f . #t ) -> result not found, but some worker is done ->  retry immediately
     ; - (#f . #f ) -> result not found, no worker is done -> retry after sleeping
     (define (try)
-      (with-synchronized
-        p
+      (with-synchronized p
         (and-let* ((j (find (lambda (j) (eq? (job-num j) num)) (poule-jobs p))))
           (cond
             ((eq? 'available (job-status j))
-             (dp "poule-result " num ": is ready")
+             (dp "poule-result " num " is ready")
              (match (job-result j)
-               ((#t . val) (cons #t val))
+               ((and (#t . val) res) res)
                ((#f . val) (signal (if (condition? val)
                                      val
                                      (condition `(exn
                                                    location worker
                                                    message ,val)))))))
-            ((not (null? (scan-workers p)))
+            ((scan-workers p)
              (dp "poule-result " num ": some worker is done, trying again...")
              (cons #f #t))
             (wait?
@@ -262,8 +274,7 @@
 
   (define (poule-dispose-results p)
     (check-poule p 'poule-dispose-results)
-    (with-synchronized
-      p
+    (with-synchronized p
       (poule-jobs-set! p
                        (remove
                          (lambda (j) (eq? 'available (job-status j)))
@@ -275,8 +286,7 @@
     (define backoff (make-backoff 0.1 1.05))
     (let loop ()
       (when
-        (with-synchronized
-          p
+        (with-synchronized p
           (scan-workers p)
           (any (lambda (j) (eq? 'pending (job-status j))) (poule-jobs p)))
         (thread-yield!)
@@ -287,8 +297,7 @@
     (dp "poule-destroy")
     (when (poule-active? p)
       (if wait? (poule-wait p))
-      (with-synchronized
-        p
+      (with-synchronized p
         (thread-specific-set! (poule-submission-thread p) #f)
         (thread-yield!)
         (let ((w (poule-workers p)))
